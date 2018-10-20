d421a855081aa1271aaf8d951c449625cbcab474
elebitzero <dave.lefaive@gmail.com>
2018-10-20 00:15:50 -0700

    Made the dropdown of occasions an array of categories.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CMPE-131';
+  readonly categories: string [] = ['Birthday', 'Christmas', 'Graduation', 'Thank you'];
 }
