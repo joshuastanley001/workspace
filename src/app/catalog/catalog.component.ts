d33b0343658fa8e12f7f4e542de8a9c489e8e606
elebitzero <dave.lefaive@gmail.com>
2018-10-21 19:01:04 -0700

    Renamed category and card-template models to follow proper naming convention.

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import {CardTemplate} from "../model/CardTemplate";
+import {CardTemplate} from '../model/card-template';
 
 @Component({
   selector: 'catalog',
